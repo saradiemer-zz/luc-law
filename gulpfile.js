41d5b4cd887dc13265f8ed222c20879d6563f9d4
sravenscraft <sravenscraft@luc.edu>
2018-02-12 12:38:27 -0600

    Added docs on root

--- gulpfile.js
+++ gulpfile.js
@@ -89,14 +89,13 @@ gulp.task('html-watch', ['index'], browserSync.reload);
 gulp.task('sass-watch', ['sass'], browserSync.reload);
 gulp.task('js-watch', ['minify'], browserSync.reload);
 
-
 gulp.task('serve', ['lint', 'sass', 'index', 'minify'], function () {
     // Serve files from the root of this project
     browserSync.init({
         server: {
             baseDir: "./public"
         }
-    }  );
+    });
     // add browserSync.reload to the tasks array to make
     // all browsers reload after tasks are complete.
     gulp.watch('./development/**/*.html', ['html-watch']);
