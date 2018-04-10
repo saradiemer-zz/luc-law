0059bbd0b5222124e09ff11fb1c8b64793308ecd
sravenscraft <sravenscraft@luc.edu>
2018-04-10 15:33:32 -0500

    Update to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -58,14 +58,6 @@ gulp.task('sass', function() {
     .pipe(gulp.dest(config.publicDir + '/css'));
 });
 
-gulp.task('sass-canvas', function() {
-    return gulp.src(config.devDir + '/scss-canvas/**/*.scss')
-    .pipe(sourcemaps.init())  // Process the original sources
-		.pipe(sass())
-		.pipe(sourcemaps.write()) // Add the map to modified source.
-    .pipe(gulp.dest(config.publicDir + '/css-canvas'));
-});
-
 gulp.task('fonts', function() {
     return gulp.src(config.devDir + '/fonts/**/*')
     .pipe(gulp.dest(config.publicDir + '/fonts'));
@@ -95,9 +87,9 @@ gulp.task('images', function(){
 
 gulp.task('html-watch', ['index'], browserSync.reload);
 gulp.task('sass-watch', ['sass'], browserSync.reload);
-gulp.task('sass-canvas-watch', ['sass-canvas'], browserSync.reload);
 gulp.task('js-watch', ['minify'], browserSync.reload);
-gulp.task('serve', ['lint', 'sass', 'sass-canvas', 'index', 'minify'], function () {
+
+gulp.task('serve', ['lint', 'sass', 'index', 'minify'], function () {
     // Serve files from the root of this project
     browserSync.init({
         server: {
